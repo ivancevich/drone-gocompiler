4440341aed1d1d8294795a2fafbffac68f582217
JC Ivancevich <jc.ivancevich@gmail.com>
2016-02-23 10:11:48 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# Docker image for the docker plugin
+#
+#     docker build --rm=true -t ivancevich/drone-gocompiler .
+
+FROM golang:1.6
+RUN go get github.com/tools/godep
+RUN CGO_ENABLED=0 go install -a std
+
+ADD drone-gocompiler /bin/
+ENTRYPOINT ["/bin/drone-gocompiler"]
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/drone/drone-plugin-go/plugin"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type Config struct {
+	Package string `json:package`
+	Output  string `json:output`
+}
+
+var (
+	buildDate string
+)
+
+func main() {
+	fmt.Printf("Drone Go Compiler Plugin built at %s\n", buildDate)
+
+	workspace := plugin.Workspace{}
+	vargs := Config{}
+
+	plugin.Param("workspace", &workspace)
+	plugin.Param("vargs", &vargs)
+	plugin.MustParse()
+
+	if len(vargs.Output) == 0 {
+		vargs.Output = "."
+	}
+
+	path := filepath.Join(workspace.Path, vargs.Package)
+	output := filepath.Join(workspace.Path, vargs.Output, vargs.Package)
+
+	cmd := exec.Command("godep", "go", "build", "-o", output)
+	cmd.Env = append(cmd.Env, "PATH=/usr/local/go/bin:$PATH")
+	cmd.Env = append(cmd.Env, "GOPATH=/drone")
+	cmd.Env = append(cmd.Env, "CGO_ENABLED=0")
+	cmd.Env = append(cmd.Env, "GO15VENDOREXPERIMENT=0")
+	cmd.Env = append(cmd.Env, "LDFLAGS='-d -w -s'")
+	cmd.Dir = path
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	trace(cmd)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+}
+
+// Trace writes each command to standard error (preceded by a ‘$ ’) before it
+// is executed. Used for debugging your build.
+func trace(cmd *exec.Cmd) {
+	fmt.Println("$", strings.Join(cmd.Args, " "))
+}
